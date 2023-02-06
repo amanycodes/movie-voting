3f5ce1d59d4c94776f0fe50268f9d485f7fc015e
bismuth42 <102381919+bismuth42@users.noreply.github.com>
2023-02-07 00:56:45 +0530

    fixed something

--- frontend/src/pages/leaderboard/Leaderboard.jsx
+++ frontend/src/pages/leaderboard/Leaderboard.jsx
@@ -6,7 +6,7 @@ const LeaderBoard = () => {
         <Paper sx={{
             padding: '7rem',
             overflow: 'auto',
-            maxHeight: '80vh',
+            maxHeight: '60vh',
             background: 'none'
         }}>
             <List sx={{
